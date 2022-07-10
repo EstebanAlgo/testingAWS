f3c433cf805028851fb1f2dcd12b3d12b169aae4
Esteban Almanza <almanza_1811@hotmail.com>
2022-07-09 23:26:47 -0500

    puerto 8443

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 var express = require("express");
 var app = express();
 
+const port = process.env.PORT || 8443;
+
 app.get("/", function (req, res) {
   res.send("Hello World!");
 }); //end app.get
 
-app.listen(443, function () {
-  console.log("Example app listening on port 3000!");
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`);
 }); //end app.listen
